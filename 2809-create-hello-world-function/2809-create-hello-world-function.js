f3180ce9556ceeb5fb9ae211c6b529d72fa0d4f5
Asif S Sheenu <asifssheenu@gmail.com>
2025-09-29 10:03:17 +0530

    Time: 29 ms (97.38%), Space: 53.9 MB (28.08%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -1,16 +1,13 @@
 /**
  * @return {Function}
  */
-  const fs = require('fs');
 
 var createHelloWorld = function() {
     
     return function(...args) {
         return "Hello World"
     }
-    process.on('exit', () => {
-    fs.writeFileSync("display_runtime.txt", "1");
-});
+ 
 };
 
 /**
