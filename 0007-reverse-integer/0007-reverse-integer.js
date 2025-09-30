71b7265202441035adb02d5fba6f6c782e79bd90
Asif S Sheenu <asifssheenu@gmail.com>
2025-09-30 13:56:50 +0530

    Time: 55 ms (39.7%), Space: 55.7 MB (47.24%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,17 @@
+var reverse = function(x) {
+    let reversed;
+
+    if (x < 0) {
+        reversed = Number(Math.abs(x).toString().split("").reverse().join("")) * -1;
+    } else {
+        reversed = Number(x.toString().split("").reverse().join(""));
+    }
+
+    if (reversed < -(2**31) || reversed > 2**31 - 1) {
+        return 0;
+    }
+
+    // Step 3: Return reversed number
+    return reversed;
+};
+
