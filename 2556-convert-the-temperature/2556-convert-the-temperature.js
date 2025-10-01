0a0ea7fa685f074752a4d48c43d948f8be635597
Asif S Sheenu <asifssheenu@gmail.com>
2025-10-01 23:22:55 +0530

    Time: 0 ms (100%), Space: 53.6 MB (62.59%) - LeetHub

---
+++ 2556-convert-the-temperature/2556-convert-the-temperature.js
@@ -0,0 +1,9 @@
+
+var convertTemperature = function(celsius) {
+     let result = []
+     let k= celsius + 273.15
+     let f= celsius * 1.80+32.00
+
+      result.push(k,f)
+      return result
+};
