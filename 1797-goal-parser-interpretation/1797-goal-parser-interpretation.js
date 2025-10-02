b5c0d158367d84b4a7a488b26630020fca0a5ab0
Asif S Sheenu <asifssheenu@gmail.com>
2025-10-02 23:18:30 +0530

    Time: 43 ms (59.48%), Space: 54 MB (43.87%) - LeetHub

---
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function (command) {
+ return command
+    .replaceAll("()", "o")
+    .replaceAll("(al)", "al")
+    return interpret
+
+};
