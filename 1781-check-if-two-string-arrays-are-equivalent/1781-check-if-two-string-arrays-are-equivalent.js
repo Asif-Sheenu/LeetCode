892a5b9d3631416a3b544fe5b63eb1c688616b3b
Asif S Sheenu <asifssheenu@gmail.com>
2025-10-13 21:07:07 +0530

    Time: 4 ms (2.31%), Space: 55.2 MB (13.87%) - LeetHub

---
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,8 @@
+
+var arrayStringsAreEqual = function(word1, word2) {
+ let result1 = word1.join("");
+    let result2 = word2.join("")
+    console.log(result1, result2)
+    return result1 === result2;
+    
+    };
