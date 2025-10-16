a1011e972d0d59e0867287bc7525ac85752a0a54
Asif S Sheenu <asifssheenu@gmail.com>
2025-10-16 11:08:28 +0530

    Time: 1053 ms (45.77%), Space: 54.1 MB (67.25%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -4,7 +4,7 @@
  */
 var checkPerfectNumber = function (num) {
     result = 0
-    for (let i = 1; i < num/2; i++) {
+    for (let i = 1; i <= num/2; i++) {
         if (num % i === 0) {
             result += i
         } 
