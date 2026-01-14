e5bb84aa58ffc91b8f5b4175b430a3fb0090bd9d
Asif S Sheenu <asifssheenu@gmail.com>
2026-01-14 14:18:53 +0530

    Time: 17 ms (7.6%), Space: 21.2 MB (6.94%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,15 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count =0
+        freq={}
+        for i in nums:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
+
+            if freq[i]>len (nums)//2:
+                return i        
+
+
+    
