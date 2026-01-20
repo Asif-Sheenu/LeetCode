5544ed52fb5f600e013e9380bf51edd5d3606d30
Asif S Sheenu <asifssheenu@gmail.com>
2026-01-20 11:59:17 +0530

    Time: 3 ms (56.53%), Space: 19.5 MB (5.9%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,14 +1,19 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        freq ={}
-        for ch in s:
-            if ch in freq:
-                freq[ch] += 1
-            else:
-                freq[ch] = 1
+            freq ={}
+            for i in s:
+                if i in freq:
+                    freq[i]+=1
+                else:
+                    freq[i]=1
+            for n in t:
+                if n in freq and freq[n]>0:
+                    freq[n] -= 1
+                else : 
+                    return (n)             
+       
 
-        for ch in t:
-            if ch in freq and freq[ch] > 0:
-                freq[ch] -= 1
-            else:
-                return ch
+
+
+            
+        
