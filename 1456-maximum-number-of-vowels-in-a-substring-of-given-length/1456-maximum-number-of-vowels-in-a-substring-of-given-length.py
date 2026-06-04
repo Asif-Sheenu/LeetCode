13409cad3723027d9966cd9aa63519025df7c291
Asif S Sheenu <asifssheenu@gmail.com>
2026-06-04 12:17:38 +0530

    Time: 51 ms (69.38%), Space: 19.8 MB (66.63%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        window=s[:k]
+        count=0
+        for i in range(k):
+            if s[i] in "aeiou":
+                count +=1
+        best=count
+        for i in range(k,len(s)):
+            if s[i] in "aeiou":
+                count +=1
+            if s[i-k] in "aeiou":
+                count -=1
+            best= max(best,count)
+        return (best)      
+        
